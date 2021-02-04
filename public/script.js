28c9ffc3273f5c55115b0d020be3102f9a76bc21
ahmad1091 <ahmadtayeb91@gmail.com>
2021-02-04 22:44:56 +0200

    create peer object to create clients ids

--- public/script.js
+++ public/script.js
@@ -1,5 +1,8 @@
 const socket = io("/");
-
+const myPerr = new Peer(undefined, {
+  host: "/",
+  port: "3001",
+});
 socket.emit("join-room", ROOM_ID, 10);
 socket.on("user-onnected", (userId) => {
   console.log(userId);
